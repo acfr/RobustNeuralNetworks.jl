46b25d98ec19eb434d107e57e5059fbd9cfdd086
nic-barbara <nbar5346@uni.sydney.edu.au>
2022-09-20 10:30:14 +1000

    Added rng to all random operations

--- src/output.jl
+++ src/output.jl
@@ -1,22 +1,19 @@
-using CUDA
-using Flux
-import Flux.gpu, Flux.cpu
-using LinearAlgebra
+"""
+$(TYPEDEF)
 
-# Linear output layer of the REN
+Linear output layer of the REN
+"""
 mutable struct Output{T}
     C2::Union{Matrix{T},CuMatrix{T}}
     D21::Union{Matrix{T},CuMatrix{T}}
     D22::Union{Matrix{T},CuMatrix{T}}
     by::Union{Vector{T},CuVector{T}}
 end
-
-# Constructor
-function Output{T}(nu::Int, nx::Int, nv::Int, ny::Int) where T
-    C2 = randn(T, ny, nx) / convert(T, sqrt(ny + nx))
-    D21 = randn(T, ny, nv) / convert(T, sqrt(ny + nv))
-    D22 = zeros(T, ny, nu)
-    by = randn(T, ny) / convert(T, sqrt(ny))
+function Output{T}(nu::Int, nx::Int, nv::Int, ny::Int; rng=Random.GLOBAL_RNG) where T
+    C2  = glorot_normal(ny,nx; rng=rng)
+    D21 = glorot_normal(ny,nv; rng=rng)
+    D22 = zeros(T, ny, nu)                          # TODO: Why zeros?
+    by  = glorot_normal(ny; rng=rng)
     return Output{T}(C2, D21, D22, by)
 end
 
@@ -27,13 +24,13 @@ Flux.trainable(layer::Output) = (layer.C2, layer.D21, layer.D22, layer.by)
 (layer::Output)(x, w, u) = layer.C2 * x + layer.D21 * w + layer.D22 * u .+ layer.by
 
 # GPU/CPU compatibility
-function Flux.gpu(M::Output{T}) where T
+function Flux.gpu(layer::Output{T}) where T
     if T != Float32
         println("Moving type: ", T, " to gpu may not be supported. Try Float32!")
     end
-    return Output{T}(gpu(M.C2), gpu(M.D21), gpu(M.D22), gpu(M.by))
+    return Output{T}(gpu(layer.C2), gpu(layer.D21), gpu(layer.D22), gpu(layer.by))
 end
 
-function Flux.cpu(M::Output{T}) where T
-    return Output{T}(cpu(M.C2), cpu(M.D21), cpu(M.D22), cpu(M.by))
+function Flux.cpu(layer::Output{T}) where T
+    return Output{T}(cpu(layer.C2), cpu(layer.D21), cpu(layer.D22), cpu(layer.by))
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,9 @@
-using Random
+"""
+    glorot_normal(n::Int, m::Int; T=Float64, rng=Random.GLOBAL_RNG)
 
-# Glorot normal functions for vectors and matrices (respectively)
-glorot_normal(n; T=Float64, rng=Random.GLOBAL_RNG) = 
-    convert.(T, randn(rng, n) / sqrt(n))
-glorot_normal(n, m; T=Float64, rng=Random.GLOBAL_RNG) = 
+Generate matrices or vectors from Glorot normal distribution
+"""
+glorot_normal(n::Int, m::Int; T=Float64, rng=Random.GLOBAL_RNG) = 
     convert.(T, randn(rng, n, m) / sqrt(n + m))
+glorot_normal(n::Int; T=Float64, rng=Random.GLOBAL_RNG) = 
+    convert.(T, randn(rng, n) / sqrt(n))
