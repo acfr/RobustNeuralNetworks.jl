7fadf5a53156f17a9ee5c15b381a40e4dfd1e2e8
nic-barbara <nbar5346@uni.sydney.edu.au>
2022-10-04 11:13:28 +1100

    Added AbstractREN type and wrote WrapREN wrapper type for RENs

--- src/RecurrentEquilibriumNetworks.jl
+++ src/RecurrentEquilibriumNetworks.jl
@@ -13,12 +13,17 @@ using Zygote: @adjoint
 import Flux.gpu, Flux.cpu
 
 
-############ Abstract type ############
+############ Abstract types ############
 
 """
 $(TYPEDEF)
 """
 abstract type AbstractRENParams{T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractREN end
 
 
 ############ Includes ############
@@ -31,6 +36,7 @@ include("Base/acyclic_ren_solver.jl")
 include("Base/direct_params.jl")
 include("Base/output_layer.jl")
 include("Base/ren.jl")
+include("Base/wrapren.jl")
 
 # Variations of REN
 include("ParameterTypes/contracting_ren.jl")
@@ -42,6 +48,7 @@ include("ParameterTypes/general_ren.jl")
 ############ Exports ############
 
 # Types
+export AbstractREN
 export AbstractRENParams
 export ContractingRENParams
 export DirectParams
@@ -49,9 +56,11 @@ export ExplicitParams
 export GeneralRENParams
 export OutputLayer
 export REN
+export WrapREN
 
 # Functions
 export init_states
 export set_output_zero!
+export update_explicit!
 
 end # end RecurrentEquilibriumNetworks
