398f2c3762be60b0e4d7917f67d1097ffc5294cd
Nic Barbara <35551982+nic-barbara@users.noreply.github.com>
2023-05-08 15:13:06 +1000

    Accidentally overwrote changes when resolving merge conflicts. Reinstated.

--- src/Base/acyclic_ren_solver.jl
+++ src/Base/acyclic_ren_solver.jl
@@ -10,8 +10,7 @@ function solve_tril_layer(ϕ::Union{typeof(Flux.relu), typeof(Flux.tanh)}, W::Ma
         Wi = @view W[i:i, 1:i - 1]
         zi = @view z_eq[1:i-1,:]
         bi = @view b[i:i, :]
-        LinearAlgebra.mul!(Wi_zi,Wi,zi)
-        @inbounds z_eq[i:i,:] .= ϕ.(Wi_zi .+ bi)       
+        z_eq[i:i,:] .= ϕ.(Wi * zi .+ bi)       
     end
     return z_eq
 end
@@ -31,8 +30,7 @@ function solve_tril_layer(ϕ::Function, W::Matrix, b::VecOrMat)
         Wi = @view W[i:i, 1:i - 1]
         zi = @view z_eq[1:i-1,:]
         bi = @view b[i:i, :]
-        LinearAlgebra.mul!(Wi_zi,Wi,zi)
-        @inbounds z_eq[i:i,:] .= ϕ.(Wi_zi .+ bi)       
+        z_eq[i:i,:] .= ϕ.(Wi * zi .+ bi)       
     end
     return z_eq
 end
