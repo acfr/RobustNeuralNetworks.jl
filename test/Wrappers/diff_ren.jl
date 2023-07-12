92743b74060d752b1580f1f8256ea142e31310a9
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-07-12 23:44:19 +1000

    Isolated the bug in an example

--- test/Wrappers/diff_ren.jl
+++ test/Wrappers/diff_ren.jl
@@ -5,15 +5,16 @@ using Random
 using RobustNeuralNetworks
 using Test
 
-# include("../test_utils.jl")
+include("../test_utils.jl")
 
 """
 Test that backpropagation runs and parameters change
 """
 batches = 10
-nu, nx, nv, ny = 4, 5, 10, 2
+nu, nx, nv, ny = 4, 5, 0, 2
 γ = 10
-model_ps = LipschitzRENParams{Float32}(nu, nx, nv, ny, γ)
+ren_ps = LipschitzRENParams{Float64}(nu, nx, nv, ny, γ)
+model = DiffREN(ren_ps)
 
 # Dummy data
 us = randn(nu, batches)
@@ -21,17 +22,22 @@ ys = randn(ny, batches)
 data = [(us[:,k], ys[:,k]) for k in 1:batches]
 
 # Dummy loss function just for testing
-function loss(model_ps, u, y)
-    m = DiffREN(model_ps)
+function loss(m, u, y)
     x0 = init_states(m, size(u,2))
     x1, y1 = m(x0, u)
     return Flux.mse(y1, y) + sum(x1.^2)
 end
 
-# Check if parameters change after a Flux update
-ps1 = deepcopy(Flux.params(model_ps))
-opt_state = Flux.setup(Adam(0.01), model_ps)
-Flux.train!(loss, model_ps, data, opt_state)
-ps2 = Flux.params(model_ps)
+# Debug batch updates
+opt_state = Flux.setup(Adam(0.01), model)
+gs = Flux.gradient(loss, model, us, ys)
+Flux.update!(opt_state, model, gs[1])
+println()
 
-@test !any(ps1 .≈ ps2)
+# # Check if parameters change after a Flux update
+# ps1 = deepcopy(Flux.params(model))
+# opt_state = Flux.setup(Adam(0.01), model)
+# Flux.train!(loss, model, data, opt_state)
+# ps2 = Flux.params(model)
+
+# @test !any(ps1 .≈ ps2)
