ac9099ac99eee80c7879de16cc52f9fda0f67d07
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-07-13 09:34:24 +1000

    Added check for zero-dim bias vectors

--- test/Wrappers/zero_dim.jl
+++ test/Wrappers/zero_dim.jl
@@ -5,26 +5,8 @@ using Random
 using RobustNeuralNetworks
 using Test
 
-function (m::AbstractREN{T})(
-    xt::AbstractVecOrMat, 
-    ut::AbstractVecOrMat,
-    explicit::ExplicitRENParams{T}
-) where T
-
-    # Allocate bias vectors to avoid error when nv = 0 or nx = 0
-    bv = (m.nv == 0) ? 0 : explicit.bv
-    bx = (m.nx == 0) ? 0 : explicit.bx
-
-    b = explicit.C1 * xt + explicit.D12 * ut .+ bv
-    wt = RobustNeuralNetworks.tril_eq_layer(m.nl, explicit.D11, b)
-    xt1 = explicit.A * xt + explicit.B1 * wt + explicit.B2 * ut .+ bx
-    yt = explicit.C2 * xt + explicit.D21 * wt + explicit.D22 * ut .+ explicit.by
-
-    return xt1, yt
-end
-
 """
-Test that backpropagation runs and parameters change
+Test that backpropagation runs when nx = 0 and nv = 0
 """
 batches = 10
 nu, nx, nv, ny = 4, 0, 0, 2
@@ -44,16 +26,8 @@ function loss(model_ps, u, y)
     return Flux.mse(y1, y) + sum(x1.^2)
 end
 
-# Test it
-model = REN(model_ps);
-x0 = init_states(model, batches)
-
-# @btime model(x0, us)
-# @btime Flux.gradient(loss, model_ps, us, ys)
-# println()
-
 # Make sure batch update actually runs
-opt_state = Flux.setup(Adam(0.01), model)
-gs = Flux.gradient(loss, model, us, ys)
-Flux.update!(opt_state, model, gs[1])
+opt_state = Flux.setup(Adam(0.01), model_ps)
+gs = Flux.gradient(loss, model_ps, us, ys)
+Flux.update!(opt_state, model_ps, gs[1])
 @test !isempty(gs)
