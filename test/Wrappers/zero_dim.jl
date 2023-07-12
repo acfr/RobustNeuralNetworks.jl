a92ea1e8c4511bdb32e3fdfbd3fa3262d34f5b63
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-07-13 09:07:09 +1000

    Found a slow fix with if statements on bias vectors

--- test/Wrappers/zero_dim.jl
+++ test/Wrappers/zero_dim.jl
@@ -5,18 +5,19 @@ using Random
 using RobustNeuralNetworks
 using Test
 
-# include("../test_utils.jl")
-
 function (m::AbstractREN{T})(
     xt::AbstractVecOrMat, 
     ut::AbstractVecOrMat,
     explicit::ExplicitRENParams{T}
 ) where T
 
-    println("testing")
-    b = explicit.C1 * xt + explicit.D12 * ut .+ explicit.bv
+    # Allocate bias vectors to avoid error when nv = 0 or nx = 0
+    bv = (m.nv == 0) ? 0 : explicit.bv
+    bx = (m.nx == 0) ? 0 : explicit.bx
+
+    b = explicit.C1 * xt + explicit.D12 * ut .+ bv
     wt = RobustNeuralNetworks.tril_eq_layer(m.nl, explicit.D11, b)
-    xt1 = explicit.A * xt + explicit.B1 * wt + explicit.B2 * ut .+ explicit.bx
+    xt1 = explicit.A * xt + explicit.B1 * wt + explicit.B2 * ut .+ bx
     yt = explicit.C2 * xt + explicit.D21 * wt + explicit.D22 * ut .+ explicit.by
 
     return xt1, yt
@@ -26,9 +27,9 @@ end
 Test that backpropagation runs and parameters change
 """
 batches = 10
-nu, nx, nv, ny, γ = 4, 5, 0, 2, 10
-ren_ps = LipschitzRENParams{Float64}(nu, nx, nv, ny, γ)
-model = DiffREN(ren_ps)
+nu, nx, nv, ny = 4, 0, 0, 2
+γ = 10
+model_ps = LipschitzRENParams{Float64}(nu, nx, nv, ny, γ)
 
 # Dummy data
 us = randn(nu, batches)
@@ -36,21 +37,23 @@ ys = randn(ny, batches)
 data = [(us, ys)]
 
 # Dummy loss function just for testing
-function loss(m, u, y)
+function loss(model_ps, u, y)
+    m = REN(model_ps)
     x0 = init_states(m, size(u,2))
     x1, y1 = m(x0, u)
     return Flux.mse(y1, y) + sum(x1.^2)
 end
 
-# Debug batch updates
+# Test it
+model = REN(model_ps);
+x0 = init_states(model, batches)
+
+# @btime model(x0, us)
+# @btime Flux.gradient(loss, model_ps, us, ys)
+# println()
+
+# Make sure batch update actually runs
 opt_state = Flux.setup(Adam(0.01), model)
 gs = Flux.gradient(loss, model, us, ys)
-println()
-
-# # Check if parameters change after a Flux update
-# ps1 = deepcopy(Flux.params(model))
-# opt_state = Flux.setup(Adam(0.01), model)
-# Flux.train!(loss, model, data, opt_state)
-# ps2 = Flux.params(model)
-
-# @test !any(ps1 .≈ ps2)
+Flux.update!(opt_state, model, gs[1])
+@test !isempty(gs)
