cfa467c03fde9cb83e59c963a8644439333de941
nic-barbara <nbar5346@uni.sydney.edu.au>
2023-08-18 14:55:20 +1000

    Minor edits

--- src/Wrappers/REN/ren.jl
+++ src/Wrappers/REN/ren.jl
@@ -75,11 +75,7 @@ function (m::AbstractREN)(xt::AbstractVecOrMat, ut::AbstractVecOrMat)
     return m(xt, ut, m.explicit)
 end
 
-function (m::AbstractREN{T})(
-    xt::AbstractVecOrMat, 
-    ut::AbstractVecOrMat,
-    explicit::ExplicitRENParams{T}
-) where T
+function (m::AbstractREN)(xt::T, ut::T, explicit::ExplicitRENParams) where T <: AbstractVecOrMat
 
     # Allocate bias vectors to avoid error when nv = 0 or nx = 0
     bv = _bias(m.nv, explicit.bv)
